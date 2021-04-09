e8ab4ddc7f2ba0c62e696ec6b7b269a17f044b3a
pragyajhala121 <73744592+pragyajhala121@users.noreply.github.com>
2021-04-09 17:46:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+function preload(){
+    song1 = loadSound("03. Bum Bum Bole.mp3");
+    song2 = loadSound("03. Choti Si Aasha.mp3");
+}
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+function modelLoaded(){
+    console.log("Posenet is Inatalized");
+}
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
+}
